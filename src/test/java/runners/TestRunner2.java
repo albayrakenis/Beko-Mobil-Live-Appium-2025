8baf06294f31d590f85c96f1610894d6320089e7
Enis <1albayrakenis@gmail.com>
2024-12-16 08:12:47 +0300

    PDP canli filtre var

--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 
 
 @CucumberOptions(
-        tags = "@pdp55",
+        tags = "@smoke",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
