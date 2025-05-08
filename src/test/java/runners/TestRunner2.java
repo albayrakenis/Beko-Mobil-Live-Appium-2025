b50563fbb6d69531e50c8a804ffb5e7a9245092b
Enis <1albayrakenis@gmail.com>
2025-05-08 09:15:49 +0300

    kredikarti25

--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 
 
 @CucumberOptions(
-        tags = "@order",
+        tags = "@kredikarti25",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
