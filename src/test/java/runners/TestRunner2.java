f70a0d3dd3042f596a94c636af12ea6ddefdfcc9
Enis ALBAYRAK <1albayrakenis@gmail.com>
2025-05-13 11:37:01 +0300

    eft

--- src/test/java/runners/TestRunner2.java
+++ src/test/java/runners/TestRunner2.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 
 
 @CucumberOptions(
-        tags = "@eft",
+        tags = "@hizliTeslimat",
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json",
